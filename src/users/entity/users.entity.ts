1e8aff2de026208f8d6b5c104321516a33f21acf
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-07-10 18:45:13 -0300

    featured: added the treatment on the users info return, subtracted the password

--- src/users/entity/users.entity.ts
+++ src/users/entity/users.entity.ts
@@ -2,7 +2,7 @@ export class User {
   id: string;
   name: string;
   email: string;
-  password: string;
+  password?: string;
   cpf: string;
   isAdmin: boolean;
   createdAt: Date;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,6 +8,15 @@ import { handleErrorConstraintUnique } from 'src/utils/handle-error-unique.util'
 
 @Injectable()
 export class UsersService {
+  private userSelect = {
+    id: true,
+    name: true,
+    email: true,
+    cpf: true,
+    isAdmin: true,
+    createdAt: true,
+    updatedAt: true,
+  };
   constructor(private readonly prisma: PrismaService) {}
 
   async create(dto: CreateUserDto): Promise<User | void> {
@@ -21,15 +30,20 @@ export class UsersService {
       isAdmin: dto.isAdmin,
     };
 
-    return this.prisma.user.create({ data }).catch(handleErrorConstraintUnique);
+    return this.prisma.user
+      .create({ data, select: this.userSelect })
+      .catch(handleErrorConstraintUnique);
   }
 
   findAll(): Promise<User[]> {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({ select: this.userSelect });
   }
 
   async verifyIdAndReturnUser(id: string): Promise<User> {
-    const user: User = await this.prisma.user.findUnique({ where: { id } });
+    const user: User = await this.prisma.user.findUnique({
+      where: { id },
+      select: this.userSelect,
+    });
 
     if (!user) {
       throw new NotFoundException(`id '${id}' not found`);
@@ -41,7 +55,7 @@ export class UsersService {
     await this.verifyIdAndReturnUser(id);
 
     return this.prisma.user
-      .update({ where: { id }, data: dto })
+      .update({ where: { id }, data: dto, select: this.userSelect })
       .catch(handleErrorConstraintUnique);
   }
 
@@ -50,7 +64,7 @@ export class UsersService {
 
     return this.prisma.user.delete({
       where: { id },
-      select: { name: true, email: true },
+      select: this.userSelect,
     });
   }
 
