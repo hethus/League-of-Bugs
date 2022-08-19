1c66b11a1e19a0a50f9d2824c0b088e1aefdde4a
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-08-19 12:44:31 -0300

    fix: fixed the deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAppStatus(): string {
-    return 'App is running! Documentation: https://league-of-bugs-production.up.railway.app/docs';
+    return 'App is running! Documentation: https://league-of-bugs.onrender.com/docs/';
   }
 }
